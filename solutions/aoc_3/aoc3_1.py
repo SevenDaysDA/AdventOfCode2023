a8af54b7359fb606400a911c7cdaaa1fc32b6f2f
SevenDaysDA <se.tariverdian@gmail.com>
2023-12-05 23:27:29 +0100

    aoc 3 solution task 1

--- solutions/aoc_3/aoc3_1.py
+++ solutions/aoc_3/aoc3_1.py
@@ -1,5 +1,9 @@
 
-file_path = 'solutions/aoc_3/input_instruction.txt' 
+
+file_path = 'solutions/aoc_3/test_case1.txt' 
+# file_path = 'solutions/aoc_3/input_aoc3_1.txt' 
+# file_path = 'solutions/aoc_3/input_instruction.txt' 
+
 
 instruction_array = []
 
@@ -7,7 +11,6 @@ try:
     with open(file_path, 'r') as file:
         lines = file.readlines()
         for line in lines:
-            print(line.strip())
             instruction_array.append(line.strip())  # Use strip() to remove leading/trailing whitespace, including '\n'
 
 except FileNotFoundError:
@@ -25,19 +28,57 @@ for instruct_id, instruct_line in enumerate(instruction_array):
     number_acc = ""
     for line_id, char in enumerate(instruct_line):
         
-        if char.isnumeric():
+        if char.isdigit():
             if not still_number:
                 begin_id = line_id
             number_acc += char
             still_number = True
         else:
             # Either symbol or dot
-            if still_number:
+            if still_number or line_id == len(instruct_line)-1:
                 end_id = line_id
                 id_map.append((instruct_id, [begin_id, end_id], number_acc))     # (line_id, [range_ids], value)
                 number_acc = ""
-                
-            still_number = False
+                still_number = False
 
 # id_map - contains all numbers
-print(id_map)
+# print(id_map)
+
+
+
+def check_relevance(map_entry, instruct_id, line_id):
+    map_row, map_row_range, _ = map_entry
+    if instruct_id >= map_row -1  and instruct_id <= map_row +1 :
+        if line_id >= map_row_range[0] -1 and line_id <= map_row_range[1]+1:
+            return True
+    return False
+
+result_value = 0
+for instruct_id, instruct_line in enumerate(instruction_array):
+    print()
+    for line_id, char in enumerate(instruct_line):
+        # print(char)
+        # Here we find a symbol:
+        if not char.isdigit() and not char == "." or char == "*":
+            print(char)
+            # print(instruct_id, line_id)
+            # Iterate through all numbers:
+            for num in id_map:
+                print(num)
+                # if char == "#":
+                #     print(num)
+                if check_relevance(num, instruct_id, line_id):
+                    # print(char)
+                    value = int(num[2])
+                    print(value)
+                    result_value += value
+
+print(result_value)
+
+
+to_low = 552835
+not_right = 567632
+to_high = 604596
+
+# 556367
+print(instruction_array)
